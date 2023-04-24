26acb7df2929e17ecd7663583d854c8aa12a1dc8
doyeonjeong <debby_@kakao.com>
2023-04-24 20:54:38 +0900

    add model: YouTubeBlocker

---
+++ ScreenTimeAPIExample/ScreenTimeAPIExample/YouTubeBlocker.swift
@@ -0,0 +1,15 @@
+//
+//  YouTubeBlocker.swift
+//  ScreenTimeAPIExample
+//
+//  Created by Doyeon on 2023/04/24.
+//
+
+import FamilyControls
+import DeviceActivity
+
+struct YouTubeBlocker {
+    func blockYouTube(completion: @escaping (Result<Void, Error>) -> Void) {
+        // TODO: 유튜브 차단 로직 구현
+    }
+}
