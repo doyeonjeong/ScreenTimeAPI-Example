d0de4d1ac3f5c5606a1761598401b482324deb49
doyeonjeong <debby_@kakao.com>
2023-04-25 12:29:34 +0900

    add: SwiftUIVIew

---
+++ ScreenTimeAPIExample/ScreenTimeAPIExample/Sources/View/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  ScreenTimeAPIExample
+//
+//  Created by Doyeon on 2023/04/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
